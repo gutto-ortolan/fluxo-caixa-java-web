d295f290854525f107bc523aab927be25c0ecbd2
gutto-ortolan <57159392+gutto-ortolan@users.noreply.github.com>
2020-06-10 09:06:03 -0300

    teste

---
+++ projetoFluxoCaixa/src/main/java/br/com/projetoFluxoCaixa/controller/LancamentoController.java
@@ -0,0 +1,60 @@
+package br.com.projetoFluxoCaixa.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import br.com.projetoFluxoCaixa.model.Lancamento;
+import br.com.projetoFluxoCaixa.model.Usuario;
+import br.com.projetoFluxoCaixa.repository.LancamentoRepository;
+
+@Controller
+public class LancamentoController {
+
+    @Autowired
+    LancamentoRepository lr;
+
+    @RequestMapping("/newLan")
+    public String newLan(HttpSession session, RedirectAttributes ra) {
+        return "newLan";
+    }
+
+
+    /* Cadastrar um lançamento no banco - apresenta falha de menu.html já foi commitado e o status code está errado
+	
+     */
+    @RequestMapping(value = "/salvarLan", method = RequestMethod.POST)
+    public String salvarLan(@RequestParam("valor") Double valor,
+            @RequestParam("descricao") String descricao,
+            @RequestParam("data") String data,
+            //@RequestParam("operacao") String operacao,
+            RedirectAttributes ra, HttpSession session) {
+        Lancamento lancamento = new Lancamento();
+        lancamento.setData(new Date());
+        lancamento.setDescricao(descricao);
+        lancamento.setOperacao("ENTRADA");
+        lancamento.setValor(valor);
+        
+        Usuario usuario = (Usuario)session.getAttribute("usuarioLogado");
+        
+        lancamento.setUsuario(usuario);
+
+        lr.save(lancamento);
+
+        System.out.println(lancamento.getValor());
+
+        List<Lancamento> lancamentoPesquisa = lr.findLancamentoPorUsuario(usuario.getIdUsuario());
+
+        ra.addFlashAttribute("lan", lancamentoPesquisa);
+        return "menu";
+
+    }
+}
